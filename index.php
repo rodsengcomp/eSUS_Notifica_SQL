7c44a14ea83353c8a01b8af4c0e2a015690241eb
Rodolfo Romaioli Ribeiro de Jesus <74135777+rodsengcomp@users.noreply.github.com>
2021-05-25 14:02:38 -0300

    Update
    Criando css

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+?>
+
+
+
+<!doctype html>
+<html lang="en" class="h-100">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>Uvis Jaçanã-Tremembé - ESUS</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="icon" href="assets/img/favicon/favicon.ico">
+    <meta name="theme-color" content="#3880ff">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/cover.css" rel="stylesheet">
+</head>
+<body class="d-flex h-100 text-center text-primary bg-light">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="mb-auto">
+        <div>
+            <h3 class="float-md-start mb-0">ESUS-NOTIFICA</h3>
+            <nav class="nav nav-masthead justify-content-center float-md-end">
+                <a class="nav-link active" aria-current="page" href="#">Início</a>
+                <a class="nav-link" href="#">Instruções</a>
+                <a class="nav-link" href="#">Contato</a>
+            </nav>
+        </div>
+    </header>
+
+    <main class="px-3">
+        <h3>Seja Bem Vindo.</h3>
+        <p class="lead">Este é mais um projeto que se inicia no setor de tecnologia da Uvis Jaçanã-Tremembé. O projeto foi criado para facilitar a atualização dos bancos de dados.</p>
+        <p class="lead">
+            <a href="#" class="btn btn-lg btn-outline-primary fw-bold">Saiba Mais</a>
+        </p>
+
+        <div class="ratio ratio-1x1">
+            <iframe src="http://10.47.171.110/phpmyadmin/db_structure.php?server=1&db=esus" title="YouTube video" allowfullscreen></iframe>
+        </div>
+    </main>
+
+    <footer class="mt-auto text-white-50">
+        <p>Desenvolvido por <a href="https://getbootstrap.com/" class="text-white">Rodolfo R. R. de Jesus</a>.</p>
+    </footer>
+</div>
